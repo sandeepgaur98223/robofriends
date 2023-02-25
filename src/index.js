a52d7dedecd7d01b4bba239e28139a61b909a772
sandeepgaur98223 <sandeepgaur98223@gmail.com>
2023-02-25 23:39:02 +0530

    last

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+//import App from './App';
+import Hello from './Hello';
 import reportWebVitals from './reportWebVitals';
+import 'tachyons'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <h1>Hello </h1>
+    <Hello greeting={"Hello"+"there"} />  
   </React.StrictMode>
 );
 
