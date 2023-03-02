f3d38629eae0d1b9ba5c885aeeed8892d36331ec
sandeepgaur98223 <sandeepgaur98223@gmail.com>
2023-03-02 19:32:53 +0530

    searchbox working completed

--- src/App.js
+++ src/App.js
@@ -1,16 +1,44 @@
-import React from 'react';
+import React,{Component} from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
 import {robots} from './robots';
 
-const App=()=>{
+class App extends Component{
+
+	constructor(){
+		super()
+		this.state={
+			robots:robots,
+			searchfield:''
+		}
+	}
+
+	onSearchChange=(event)=>{
+		this.setState({searchfield:event.target.value})
+
+		}
+	
+
+render()
+{
+		const filteredRobots=this.state.robots.filter(robots=>{
+			return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+
+		}
+			)
+
+		console.log(filteredRobots);
+
 	return (
 		<div className='tc'>
 		<h1>Robofriends</h1>
-		<SearchBox />
-		<CardList robots={robots}/>
+		<SearchBox searchChange={this.onSearchChange}/>
+		<CardList robots={filteredRobots}/>
 		</div>
 		);
+
 }
+}
+
 
 export default App;
