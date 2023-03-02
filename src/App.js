73872976e3203f2ee0dcb4ec4cec5f33aebcb560
sandeepgaur98223 <sandeepgaur98223@gmail.com>
2023-03-02 19:59:30 +0530

    css also done

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React,{Component} from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
 import {robots} from './robots';
+import './App.css';
 
 class App extends Component{
 
@@ -31,7 +32,7 @@ render()
 
 	return (
 		<div className='tc'>
-		<h1>Robofriends</h1>
+		<h1 className='f1'>Robofriends</h1>
 		<SearchBox searchChange={this.onSearchChange}/>
 		<CardList robots={filteredRobots}/>
 		</div>
