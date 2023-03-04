fbdaa03b622b3d7de6b5c0da682833a5323c62eb
sandeepgaur98223 <sandeepgaur98223@gmail.com>
2023-03-04 15:33:36 +0530

    folder structure and build done

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,7 @@
 import React,{Component} from 'react';
-import CardList from './CardList';
-import SearchBox from './SearchBox';
-import Scroll from './Scroll';
+import CardList from '../components/CardList';
+import SearchBox from '../components/SearchBox';
+import Scroll from '../components/Scroll';
 //import {robots} from './robots';
 import './App.css';
 
@@ -33,8 +33,8 @@ class App extends Component{
 
 render()
 {
-		const filteredRobots=this.state.robots.filter(robots=>{
-			return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+		const filteredRobots=this.state.robots.filter(robot=>{
+			return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
 
 		}
 			)
